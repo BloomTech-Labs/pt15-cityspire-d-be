69ad72e77586560425bdb27a755ba4b068731f2b
Troy Moore <tbmoore85@gmail.com>
2021-01-27 22:04:26 -0500

    Work in progress to debug

--- data/migrations/20210125232920_locations.js
+++ data/migrations/20210125232920_locations.js
@@ -1,8 +1,20 @@
 
 exports.up = function(knex) {
-  
+  return knex.schema
+
+  .createTable('locations', tbl =>{
+      tbl.increments('id').notNullable().unique().primary();
+      tbl.string('lat').notNullable();
+      tbl.string('lon').notNullable();
+      tbl.string('crime_data');
+      tbl.integer('population');
+      tbl.integer('cost_of_living');
+      tbl.integer('rental_rates');
+      tbl.integer('walk_score');
+
+  })
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTableIfExists('locations')
 };
